3151a276843340affee22ffe3dce4c275335d91f
gauribonde13 <137591363+gauribonde13@users.noreply.github.com>
2024-08-15 13:03:38 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+function Footer() {
+  return (
+    <div className='footer'>
+        <h2>Gauri !</h2>
+        <p>@copyright code with Gauri</p>
+    </div>
+  )
+}
+
+export default Footer
